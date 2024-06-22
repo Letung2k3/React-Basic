1cc3e9b8ebe7e30b1af72df6f138c68c29734dc9
Le Thanh Tung <letung767768@gmail.com>
2024-06-22 19:53:26 +0700

    Da thay doi

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MyComponent from './Example/MyComponent';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hello world ll
+         hello world lp
         </p>
         <a
           className="App-link"
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <MyComponent/>
+        </div>
       </header>
     </div>
   );
