cb36127104742d9f8fa047c6560acd69339e7183
Le Thanh Tung <letung767768@gmail.com>
2024-06-23 18:00:15 +0700

    branch 2

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,37 @@
 import React from "react";
 class MyComponent extends React.Component{
+    /*
+    jsx:html in block function
+    fragment: <> </> react
+    state: is a object which help update đata intermidiate, you dont need refresh a website
+            (cập nhật trạng thái)
+    state = {}
+    note : don't use keyword""Function when you call function"
+    */
+   state = {
+        name:"Dotaq",
+        origin:"Double"
+   }
+    handleOnchange(event) {
+        return this.setState({
+            name:event.target.value
+        })
+   }
     render(){
+    
         return(
-            <div>Hello world!</div>
+          <>
+            <div>
+               <input value={this.state.name} type="text"
+                onChange={(event)=>this.handleOnchange(event)}
+               />
+                Hello world! Ben {this.state.name}
+            </div>
+             
+             <div>
+                {this.state.origin}
+            </div>
+          </>
         )  
     }
 }
