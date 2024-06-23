52a6e1a16a2e0d2af9550d0eb6190a4c20bb751c
Le Thanh Tung <letung767768@gmail.com>
2024-06-23 21:49:25 +0700

    23/6 replace 3

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,25 @@
 import React from "react";
 class MyComponent extends React.Component{
+    state = {
+        name:"Dotatq",
+        key:"K01"
+    }
+    handleEventState = (event)=>{
+         this.setState({
+            name:event.target.value
+         })
+    }
     render(){
         return(
-            <div>Hello world!</div>
+            <>
+                <div>
+                    <input onClick={(e)=>this.handleEventState(e)} type="text" value={this.state.name}/>
+                    {this.state['name']}
+                </div>
+                <div>
+                    {this.state['key']}
+                </div>
+            </>
         )  
     }
 }
